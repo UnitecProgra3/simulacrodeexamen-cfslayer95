cae78b7e4a1a1b720fea49d1b9426ae797db40e3
Turupawn <ahmed.hn.43@gmail.com>
2014-11-05 07:24:55 -0600

    fix evaulador

--- Evaluador.cpp
+++ Evaluador.cpp
@@ -30,6 +30,45 @@ void evaluar()
         cout<<"Incorrecto"<<endl;
     }
 
+
+    cout<<"esMayuscula:\t\t";
+    if(!esMayuscula('a')
+        && !esMayuscula('b')
+        && !esMayuscula('w')
+        && !esMayuscula('y')
+        && !esMayuscula('a')
+        && esMayuscula('Z')
+        && esMayuscula('E')
+        && esMayuscula('A')
+        )
+    {
+        nota+=2.5;
+        cout<<"Correcto"<<endl;
+    }else
+    {
+        cout<<"Incorrecto"<<endl;
+    }
+
+    int a=2;
+    int b=4;
+    int c=10;
+    int d=15;
+
+    elevarAlCuadrado(&a);
+    elevarAlCuadrado(&b);
+    elevarAlCuadrado(&c);
+    elevarAlCuadrado(&d);
+
+    cout<<"elevarAlCuadrado:\t";
+    if(a==4 && b==16 && c==100 && d==225)
+    {
+        nota+=2.5;
+        cout<<"Correcto"<<endl;
+    }else
+    {
+        cout<<"Incorrecto"<<endl;
+    }
+
     cout<<"getMayor:\t\t";
     if(getMayor(&a,&b,&c)==c
         && getMayor(&a,&b,&a)==b
